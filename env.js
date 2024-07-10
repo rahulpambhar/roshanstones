2aaae81777184c8566d1643d7b81c67ea653ac71
rahul pambhar <105906859+rahulpambhar@users.noreply.github.com>
2024-07-10 16:15:42 +0530

    conf changes

--- env.js
+++ env.js
@@ -1,5 +1,7 @@
-export const apiUrl = "http://localhost:3000/api";
-export const baseUrl = "http://localhost:3000/";
+// export const apiUrl = "http://localhost:3000/api";
+// export const baseUrl = "http://localhost:3000/";
+export const apiUrl = "https://roshanstones.vercel.app//api";
+export const baseUrl = "https://roshanstones.vercel.app//";
 
 export const orderPrifix = "ORD"
 export const tempOrderPrifix = "TEMP_ORD"
